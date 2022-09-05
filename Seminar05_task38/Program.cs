b5a234ada4df69296d7e5e6d4e2c9f7f1282235e
«Khristina» <abramova_khristina@internet.ru>
2022-09-05 13:54:53 +0300

    добавлен файл семинар 5 задание 38

---
+++ Seminar05_task38/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 38: Задайте массив вещественных чисел (вещественные числа – дабл). Найдите разницу между максимальным и инимальным элементов массива. [3 7 22 2 78] -> 76
+
+Console.Clear(); //очистка терминала 
+Console.Write("Введите размер массива  ");
+int size = int.Parse(Console.ReadLine());
+double[] numbers = new double[size];
+FillArrayRandomNumbers(numbers);
+Console.Write("В массиве -> " );
+PrintArray(numbers); 
+double min = int.MaxValue;
+double max = int.MinValue; 
+void FillArrayRandomNumbers(double[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = new Random().Next(1,100);;
+        }
+}
+void PrintArray(double[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
+for (int z = 0; z < numbers.Length; z++)
+{
+    if (numbers[z] > max)
+        {
+            max = numbers[z];
+        }
+    if (numbers[z] < min)
+        {
+            min = numbers[z];
+        }
+}
+
+Console.WriteLine($"Максимальное число = {max}, минимальное число = {min}, разница между ними = {max - min}");
