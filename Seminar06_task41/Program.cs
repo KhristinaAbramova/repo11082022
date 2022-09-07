301cf12c0d420bb932642992b87a873dcab51482
«Khristina» <abramova_khristina@internet.ru>
2022-09-07 14:13:11 +0300

    добавлен файл семинар 6 задание 41

---
+++ Seminar06_task41/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear(); //очистка терминала
+Console.Write("Введите колличество чисел: ");
+int m = int.Parse(Console.ReadLine());
+int count = 0;
+for (int i = 0; i < m; i++)
+{
+    Console.Write($"Число {i + 1}: ");
+    int x = int.Parse(Console.ReadLine());
+    if (x > 0)
+        count++;
+}
+Console.WriteLine($"Положительные: {count}");
+
