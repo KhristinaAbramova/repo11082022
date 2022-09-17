9c9b2249dde3cddfb8d42f18c67b23fb94d8a091
«Khristina» <abramova_khristina@internet.ru>
2022-09-17 18:32:57 +0300

    добавлен файл семинар 9 задание 64

---
+++ Seminar09_task64/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+M = 4; N = 8. -> ""4, 6, 7, 8"" */
+
+Console.Clear();
+Console.Write("Введите значение M: ");
+int N = int.Parse(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int M = int.Parse(Console.ReadLine());
+Console.Write($"Натуральные числа в промежутке от {N} до {M} -> ");
+    if (M>N)
+        for (int i = N; i <= M; i++)
+        Console.Write($" {i}");  
+        else
+        for (int i = M; i <= N; i++)
+        Console.Write($" {i}");
+
+
+        
